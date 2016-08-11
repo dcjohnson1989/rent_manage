1523b8afdb13202a33d10b29f26b129d6a230e91
Johnson_Mac <dcjohnson1989gmail.com>
2016-08-12 00:12:06 +0800

    update final framework

--- flaskr.py
+++ flaskr.py
@@ -15,6 +15,16 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
+def get_db():
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
+
 @app.before_request
 def before_request():
 	g.db = connect_db()
@@ -28,9 +38,38 @@ def teardown_request(exception):
 @app.route('/')
 def show_entries():
 	cur = g.db.execute('select title, text from entries order by id desc')
-	entries = [dict(title=row[0], text=rowp[1]) for row in cur.fetchall()]
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
 	return render_template('show_entries.html', entries=entries)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('login_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
 	app.run()
