f5c0c4bbfee4897e8e51d33e05e89b2e98eb957b
Johnson Zhong <Johnson_Zhong@epam.com>
2016-08-12 17:47:49 +0800

    rent system basic framework set up

--- flaskr.py
+++ flaskr.py
@@ -2,7 +2,7 @@ import sqlite3
 
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-DATABASE = 'flaskr.db'
+DATABASE = 'rent.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
@@ -37,16 +37,22 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-	cur = g.db.execute('select title, text from entries order by id desc')
-	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-	return render_template('show_entries.html', entries=entries)
+	cur = g.db.execute('select room_id, rent_price, pay_date, water_fee from rent_payment order by room_id desc')
+	rents = [dict(room_id=row[0], rent_price=row[1], pay_date=row[2], water_fee=row[3]) for row in cur.fetchall()]
+	return render_template('show_rent.html', rents=rents)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
 	if not session.get('logged_in'):
 		abort(401)
 	db = get_db()
-	db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+
+	room_id = int(request.form['room_id'])
+	rent_price =  int(request.form['rent_price'])
+	pay_date = request.form['pay_date']
+	water_fee = float(request.form['water_fee'])
+
+	db.execute('insert into rent_payment (room_id, rent_price, pay_date, water_fee) values (?, ?, ?, ?)', [room_id, rent_price, pay_date, water_fee])
 	db.commit()
 	flash('New entry was successfully posted')
 	return redirect(url_for('show_entries'))
--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 drop table if exists entries;
-create table entries (
-	id integer primary key autoincrement,
-	title text not null,
-	text text not null
+create table rent_payment (
+	room_id integer primary key autoincrement,
+	rent_price integer not null,
+	pay_date datetime not null,
+	water_fee real not null
 );
